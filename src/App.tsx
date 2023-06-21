31f243b3d4b98fffd884b506cfdf4796f780abb2
RicardoHungJS <ricardohung92@gmail.com>
2023-06-21 14:34:09 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import HeaderComponent from './components/header/header'
+
+function App() {
+  return (
+    <>
+      <HeaderComponent />
+      <h1>Hola mundo</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/header.tsx
@@ -0,0 +1,7 @@
+const HeaderComponent = () => {
+    return (
+        <h1>Soy el header</h1>
+    )
+}
+
+export default HeaderComponent;
